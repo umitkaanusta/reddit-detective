6ac509e28c7068483fb8ef98dc7f90f50a81a8f9
Ümit Kaan Usta <u.kaanusta@gmail.com>
2020-11-05 18:47:49 +0300

    Added cyborg score metric

--- reddit_detective/analytics/metrics.py
+++ reddit_detective/analytics/metrics.py
@@ -1,6 +1,7 @@
 from neo4j import BoltDriver
 
-from reddit_detective.analytics.utils import get_redditors
+from reddit_detective.analytics.utils import (get_redditors, get_user_comments_times,
+    get_submission_comments_times, get_subreddit_comments_times)
 
 
 def interaction_score(driver: BoltDriver, username):
@@ -35,3 +36,42 @@ def interaction_score_normalized(driver: BoltDriver, username):
     users_score = interaction_score(driver, username)
     total_score = sum([interaction_score(driver, user) for user in get_redditors(driver)])
     return users_score / total_score
+
+
+def _cyborg_score(driver: BoltDriver, name, util_func) -> tuple:
+    """
+    Calculates the ratio of cyborg-like comments to all comments of the user
+    Tuple's first element is the score, second element is a list of
+    ids of the cyborg-like comments.
+
+    Inspired from "Analyzing behavioral trends in community driven
+    discussion platforms like Reddit"
+    DOI: 10.1109/ASONAM.2018.8508687
+
+    At a subreddit, 17%-20% of the people exhibit such cyborg-like behaviors.
+    If a post's first comment is made within 6 seconds, the chances
+    of it being cyborg-like is 79%-83.9% according to the paper.
+    This information is extracted by looking at the character sizes of those
+    comments.
+
+    Cyborg-like comments can also be an advertisement,
+    AutoModerator post or a copy-paste.
+    """
+    cyborg_comms = []
+    ids, times = util_func(driver, name)
+    for i in range(len(ids)):
+        if times[i] <= 6:
+            cyborg_comms.append(ids[i])
+    return len(cyborg_comms) / len(ids), cyborg_comms
+
+
+def cyborg_score_user(driver: BoltDriver, username):
+    return _cyborg_score(driver, username, util_func=get_user_comments_times)
+
+
+def cyborg_score_submission(driver: BoltDriver, submission_id):
+    return _cyborg_score(driver, submission_id, util_func=get_submission_comments_times)
+
+
+def cyborg_score_subreddit(driver: BoltDriver, subreddit_name):
+    return _cyborg_score(driver, subreddit_name, util_func=get_subreddit_comments_times)
--- reddit_detective/analytics/utils.py
+++ reddit_detective/analytics/utils.py
@@ -1,9 +1,43 @@
 from neo4j import BoltDriver
+from collections import OrderedDict
 
 
 def get_redditors(driver: BoltDriver) -> list:
     s = driver.session()
     users = list(s.run("""
-MATCH(r:Redditor) WITH r RETURN r.username
+MATCH (r:Redditor) WITH r RETURN r.username
 """))
     return [user[0] for user in users]
+
+
+def get_user_comments_times(driver: BoltDriver, username):
+    s = driver.session()
+    comments = list(s.run("""
+MATCH (:Redditor {username: "%s"})-[:AUTHORED]-(c:Comment)-[:UNDER]-(s:Submission)
+WITH c, s
+RETURN c.id AS id, (c.created_utc - s.created_utc) / 1000 AS seconds_past
+""" % username))
+    comments = dict(comments)
+    return list(comments.keys()), list(comments.values())
+
+
+def get_submission_comments_times(driver: BoltDriver, submission_id):
+    s = driver.session()
+    comments = list(s.run("""
+MATCH (s:Submission {id: "%s"})-[:UNDER]-(c:Comment)
+WITH c, s
+RETURN c.id AS id, (c.created_utc - s.created_utc) / 1000 AS seconds_past
+""" % submission_id))
+    comments = dict(comments)
+    return list(comments.keys()), list(comments.values())
+
+
+def get_subreddit_comments_times(driver: BoltDriver, subreddit_name):
+    s = driver.session()
+    comments = list(s.run("""
+MATCH (:Subreddit {name: "%s"})-[:UNDER]-(s:Submission)-[:UNDER]-(c:Comment)
+WITH c, s
+RETURN c.id AS id, (c.created_utc - s.created_utc) / 1000 AS seconds_past
+""" % subreddit_name))
+    comments = dict(comments)
+    return list(comments.keys()), list(comments.values())
