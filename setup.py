2ac0ef612e4b77a7dc70d302e4d55347779efe2f
Ümit Kaan Usta <u.kaanusta@gmail.com>
2020-11-07 14:51:27 +0300

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,39 @@
-from distutils.core import setup
+import setuptools
 
 from reddit_detective import VERSION
 
-setup(
-    name='reddit-detective',
-    packages=['reddit_detective', 'reddit_detective.analytics'],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="reddit-detective",
     version=VERSION,
-    license='MIT',
-    description='Play detective on Reddit',
-    author='Ümit Kaan Usta',
-    author_email='u.kaanusta@gmail.com',
-    url='https://github.com/umitkaanusta/reddit-detective',
-    download_url=f'https://github.com/umitkaanusta/reddit-detective/archive/v{VERSION}.tar.gz',
+    author="Ümit Kaan Usta",
+    author_email="u.kaanusta@gmail.com",
+    url="https://github.com/umitkaanusta/reddit-detective",
+    description="Play detective on Reddit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
     keywords=["reddit", "data", "analysis", "analytics", "social",
               "network", "graph", "neo4j", "media", "news", "politics",
               "campaign", "information", "troll", "comment", "influencer",
               "idea", "spread", "campaign", "elections"],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Sociology',
-        'Topic :: Education',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Sociology",
+        "Topic :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
+    python_requires=">=3.6"
 )
