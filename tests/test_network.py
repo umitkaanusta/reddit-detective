5262f45243b5979cc0b2577685076d112c644b68
Ümit Kaan Usta <u.kaanusta@gmail.com>
2021-02-21 04:56:47 +0300

    Update test_network.py

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,16 +1,18 @@
 from neo4j import GraphDatabase
+from pprint import pprint
+from collections import Counter
 
 from tests import api_
-from reddit_detective import RedditNetwork, Comments
-from reddit_detective.data_models import Redditor
+from reddit_detective import RedditNetwork, Comments, CommentsReplies
+from reddit_detective.data_models import Redditor, Submission
 
 driver_ = GraphDatabase.driver(
     "bolt://localhost:7687",
-    auth=("neo4j", "testing")
+    auth=("neo4j", "test2")
 )
 
 
-def test_network():
+def test_network_creation():
     net = RedditNetwork(
         driver=driver_,
         components=[
@@ -23,8 +25,28 @@ def test_network():
     net.run_cypher_code()
 
 
+def test_code_uniqueness():
+    obj = CommentsReplies(Submission(api_, "jpt7s7", limit=None))
+    net = RedditNetwork(
+        driver=driver_,
+        components=[
+            obj
+        ]
+    )
+    obj_code_list = obj.code()
+    net_code_list = list(net._codes())
+    # if a submission's author has comments authored by themselves,
+    # merges of each such person will be 2 in obj_code_list and 1 in net_code_list
+    # that's why we do val <= 1
+    # we'll avoid that extra merge too in the coming update
+    assert all(
+        val <= 1 for val in (Counter(obj_code_list) - Counter(net_code_list)).values()
+    )
+
+
 def run():
-    test_network()
+    test_code_uniqueness()
+    # test_network_creation()
 
 
 if __name__ == '__main__':
